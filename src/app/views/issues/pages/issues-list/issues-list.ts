72ab74225ec96877e2b358a0ee1cb14dbb9aed71
Judith Sitetive <50967294+Judithjbn@users.noreply.github.com>
2026-07-09 00:44:30 +0200

    feat: integrar componete Card en la lista de issues con navegación al detail. Y utilizar projección de contenido

--- src/app/views/issues/pages/issues-list/issues-list.ts
+++ src/app/views/issues/pages/issues-list/issues-list.ts
@@ -2,16 +2,22 @@ import { Component, inject } from '@angular/core';
 import { Header } from "@shared/components/header/header";
 import { Button } from "@shared/components/button/button";
 import { IssuesFacade } from '../../facades/issues.facade';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 import { Spinner } from "@/app/shared/components/spinner/spinner";
 import { NgStyle } from "@angular/common";
+import { Card } from '@/app/shared/components/card/card';
 
 @Component({
   selector: 'app-issues-list',
-  imports: [Header, Button, RouterLink, Spinner, NgStyle],
+  imports: [ Card, Button, RouterLink, Spinner, NgStyle],
   templateUrl: './issues-list.html',
   styleUrl: './issues-list.css',
 })
 export class IssuesList {
   public readonly issuesFacade = inject(IssuesFacade);
+  private readonly _router = inject(Router);
+
+  public goToDetail(issueNumber: number): void{
+    this._router.navigate(['/issues-detail', issueNumber]);
+  }
 }
