a87e0cced10035f11c22076a014f7ee97769f74d
Judith Sitetive <50967294+Judithjbn@users.noreply.github.com>
2026-07-21 18:09:50 +0200

    feat: agregar variantes al componente Button (primary, secondary, ghost)

--- src/app/shared/components/button/button.ts
+++ src/app/shared/components/button/button.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   imports: [],
   templateUrl: './button.html',
   styleUrl: './button.css',
+  
 })
-export class Button {}
+export class Button {
+  public readonly variant = input<'primary' | 'secondary'|'ghost'>('primary');
+  public readonly variantClass = computed(() =>
+     `btn-app-${this.variant()}`);
+}
